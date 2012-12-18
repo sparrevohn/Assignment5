8af28734a6b904c1396c248ce3d02bc05dc7d391
hugstikdog <hugstikdog@PC>
2012-12-18 14:31:18 +0100

    Added files

---
+++ Assignment5/src/spreadsheet/Application.java
@@ -0,0 +1,26 @@
+package spreadsheet;
+
+import java.util.ArrayList;
+
+/** A singleton class representing a spreadsheet application.
+ *
+ * The instance is initialized on first mention of the class.
+ */
+public final class Application {
+
+  private ArrayList<Spreadsheet> spreadsheets;
+  private Spreadsheet worksheet;
+
+  public static final Application instance = new Application();
+
+  private Application() {
+    this.worksheet = new Spreadsheet();
+    this.spreadsheets = new ArrayList<Spreadsheet>();
+    this.spreadsheets.add(this.worksheet);
+  }
+
+  public void exit() {
+    System.exit(0);
+  }
+
+}
---
+++ Assignment5/src/ui/command/Command.java
@@ -0,0 +1,7 @@
+package ui.command;
+
+public abstract class Command {
+
+  public abstract void execute();
+
+}
