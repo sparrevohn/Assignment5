4c1e9a322eabcacd7540486210d17184f2fc95f2
hugstikdog <hugstikdog@PC>
2012-12-18 16:03:55 +0100

    Made Application

--- Assignment5/src/spreadsheet/Application.java
+++ Assignment5/src/spreadsheet/Application.java
@@ -10,13 +10,50 @@ public final class Application {
 
   private ArrayList<Spreadsheet> spreadsheets;
   private Spreadsheet worksheet;
-
+  private ArrayList<String> sheetNames;
+  
   public static final Application instance = new Application();
 
   private Application() {
     this.worksheet = new Spreadsheet();
     this.spreadsheets = new ArrayList<Spreadsheet>();
     this.spreadsheets.add(this.worksheet);
+    this.sheetNames = new ArrayList<String>();
+    this.sheetNames.add(this.worksheet.getName());
+  }
+  
+  public Spreadsheet getWorksheet() {
+	  return worksheet;
+  }
+  
+  public void newSpreadsheet() {
+	  spreadsheets.add(new Spreadsheet());
+	  String str = spreadsheets.get(spreadsheets.size() - 1).getName();
+	  sheetNames.add(str);
+	  }
+  
+  public void changeWorksheet(final String name) 
+  throws NoSuchSpreadsheetException {
+	  for (Spreadsheet sheet : spreadsheets) {
+		  if (sheet.equals(name)) {
+			  worksheet = sheet;
+		  return;
+		  }
+	  }
+	  throw new NoSuchSpreadsheetException(name);
+  }
+  
+  public Spreadsheet getSpreadsheet(final String name) 
+  throws NoSuchSpreadsheetException {
+	  for (Spreadsheet sheet : spreadsheets) {
+		  if (name.equals(sheet.getName())) 
+			return sheet;
+	  }
+	  throw new NoSuchSpreadsheetException(name);
+  }
+  
+  public Iterable<String> listSpreadsheets() {
+	  return sheetNames;
   }
   
   public void exit() {
